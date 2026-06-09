6f28f3b39b50232011e6b643e3b209fedc662ade
Luco1421 <byluco1421@gmail.com>
2026-06-09 00:31:32 -0600

    falta correr completo

--- modules/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Modulos del pipeline de segmentacion oftalmologica."""
-
-from .medgemma_conditioner import MedGemmaConditioner
-from .medgemma_runtime import MedGemmaRuntime
-
-__all__ = ["MedGemmaConditioner", "MedGemmaRuntime"]
